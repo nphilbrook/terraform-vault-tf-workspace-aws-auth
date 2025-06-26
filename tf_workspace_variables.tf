3325e610d537ced12a6ab292f7823ee158557b2e
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2025-06-26 13:38:55 -0400

    Use the account number as the tag

--- tf_workspace_variables.tf
+++ tf_workspace_variables.tf
@@ -1,8 +1,3 @@
-/* data "tfe_workspace" "workspace" {
-  name         = var.workspace_name
-  organization = var.tf_organization
-}
- */
 # Primary AWS Account - Using the first account in the list
 locals {
   primary_aws_account_id     = var.aws_account_ids[0]
@@ -37,33 +32,32 @@ resource "tfe_variable" "vault_backed_aws_run_role_arn" {
 
 # Additional AWS accounts with tags
 resource "tfe_variable" "vault_backed_aws_additional_role_arns" {
-  for_each = { for idx, account_id in local.additional_aws_account_ids : account_id => "ACCT${idx + 1}" }
+  for_each = toset(local.additional_aws_account_ids)
 
   key          = "TFC_VAULT_BACKED_AWS_RUN_ROLE_ARN_${each.value}"
-  value        = "arn:aws:iam::${each.key}:role/${var.aws_iam_role_name}"
+  value        = "arn:aws:iam::${each.value}:role/${var.aws_iam_role_name}"
   category     = "env"
-  description  = "ARN of the AWS role to assume for account ${each.key}"
+  description  = "ARN of the AWS role to assume for account ${each.value}"
   workspace_id = var.workspace_id
 }
 
 # For tagged accounts, we need to set the auth type and vault role as well
 resource "tfe_variable" "vault_backed_aws_auth_additional" {
-  for_each = { for idx, account_id in local.additional_aws_account_ids : account_id => "ACCT${idx + 1}" }
+  for_each = toset(local.additional_aws_account_ids)
 
   key          = "TFC_VAULT_BACKED_AWS_AUTH_${each.value}"
   value        = "true"
   category     = "env"
-  description  = "Enable Vault-backed AWS authentication for account ${each.key}"
+  description  = "Enable Vault-backed AWS authentication for account ${each.value}"
   workspace_id = var.workspace_id
 }
 
 resource "tfe_variable" "vault_backed_aws_run_vault_role_additional" {
-  for_each = { for idx, account_id in local.additional_aws_account_ids : account_id => "ACCT${idx + 1}" }
+  for_each = toset(local.additional_aws_account_ids)
 
   key          = "TFC_VAULT_BACKED_AWS_RUN_VAULT_ROLE_${each.value}"
   value        = vault_aws_secret_backend_role.vault_aws_role.name
   category     = "env"
-  description  = "Vault role for AWS authentication for account ${each.key}"
+  description  = "Vault role for AWS authentication for account ${each.value}"
   workspace_id = var.workspace_id
 }
-
